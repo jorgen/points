a3dbfc11c09d672fb3e509aeadcd2ae5f646176b
Jørgen Lind <jorgen.lind@gmail.com>
2021-01-20 00:20:37 +0100

    Make building external projects better

--- CMake/Build3rdParty.cmake
+++ CMake/Build3rdParty.cmake
@@ -1,10 +1,28 @@
-function(Build3rdParty)
+include(BuildExternal)
+include(GetPackageInstallDir)
+
+macro(Build3rdParty)
+  list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/CMake/FindPackage/SDL2)
+  GetPackageInstallDir(SDL2_INSTALL_DIR sdl_build ${sdl_VERSION})
+  Find_Package(SDL2 REQUIRED)
+  BuildExternalCMake(sdl_build ${sdl_VERSION} ${sdl_SOURCE_DIR} "" "SDL2::SDL2;SDL2::SDL2main")
+  
+#  BuildExternalCMake(libuv_build ${libuv_VERSION} ${libuv_SOURCE_DIR} LIBUV_ROOT "")
+#  message("curl ${curl_VERSION} ${curl_SOURCE_DIR}")
+#  BuildExternalCMake(curl_build ${curl_VERSION} ${curl_SOURCE_DIR} CURL_ROOT "")
   set(OLD_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
-  add_subdirectory(${sdl_SOURCE_DIR})
   set(BUILD_SHARED_LIBS OFF)
   add_subdirectory(${fmt_SOURCE_DIR})
   add_subdirectory(${glm_SOURCE_DIR})
   add_subdirectory(${catch2_SOURCE_DIR})
   set(BUILD_SHARED_LIBS ${OLD_BUILD_SHARED_LIBS})
   include(${cmakerc_SOURCE_DIR}/CMakeRC.cmake)
-endfunction()
+  
+  add_custom_target(build_3rd_party
+    DEPENDS
+      sdl_build
+      #libuv_build
+      #curl_build
+  )
+
+endmacro()
---
+++ CMake/BuildExternal.cmake
@@ -0,0 +1,44 @@
+include(GetPackageInstallDir)
+function(BuildExternalCMake name version source_dir cmake_args build_targets)
+  GetPackageInstallDir(INSTALL_INT_CONFIG ${name} ${version})
+  set(CMAKE_BUILD_TYPE_ARG "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE};")
+
+  if (cmake_args)
+    set(cmake_arg_complete "${cmake_args};")
+  endif()
+  set(cmake_arg_complete "${cmake_arg_complete}${CMAKE_BUILD_TYPE_ARG}")
+
+  if (CCACHE_PROGRAM)
+    set(cmake_arg_complete "${cmake_arg_complete}-DCMAKE_C_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER_LAUNCHER=ccache;")
+  endif()
+
+  set(cmake_arg_complete "${cmake_arg_complete}-DCMAKE_INSTALL_PREFIX=${INSTALL_INT_CONFIG};-DCMAKE_INSTALL_MESSAGE=LAZY")
+
+  foreach(build_target ${build_targets})
+    if (TARGET ${build_target})
+	  get_target_property(target_type ${build_target} TYPE)
+      if (WIN32 AND (target_type STREQUAL SHARED_LIBRARY))
+        get_target_property(imported_implig_debug ${build_target} BUILD_EXTERNAL_IMPORTED_IMPLIB_DEBUG)
+        list(APPEND build_byproducts ${imported_implig_debug})
+        get_target_property(imported_implig_release ${build_target} BUILD_EXTERNAL_IMPORTED_IMPLIB_RELEASE)
+        list(APPEND build_byproducts ${imported_implig_release})
+      else()
+        get_target_property(imported_location_debug ${build_target} BUILD_EXTERNAL_IMPORTED_LOCATION_DEBUG)
+        list(APPEND build_byproducts ${imported_location_debug})
+        get_target_property(imported_location_release ${build_target} BUILD_EXTERNAL_IMPORTED_LOCATION_RELEASE)
+        list(APPEND build_byproducts ${imported_location_release})
+      endif()
+    endif()
+  endforeach()
+
+  include(ExternalProject)
+  ExternalProject_Add(${name}
+    PREFIX ${PROJECT_BINARY_DIR}/${name}_${version}
+    SOURCE_DIR ${source_dir}
+    BUILD_IN_SOURCE OFF
+    CMAKE_GENERATOR ${CMAKE_GENERATOR}
+    CMAKE_GENERATOR_PLATFORM ${CMAKE_GENERATOR_PLATFORM}
+    CMAKE_ARGS ${cmake_arg_complete}
+    BUILD_BYPRODUCTS ${build_byproducts})
+endfunction()
+ 
