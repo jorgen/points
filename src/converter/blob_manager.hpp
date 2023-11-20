d756d43d1892bc9f4339c173d6ffded3220fa756
Jørgen Lind <jorgen.lind@gmail.com>
2023-11-20 01:12:49 +0100

    Add paging to blob_manager_t
    Next is adding a free list making finding free space cheap

--- src/converter/blob_manager.hpp
+++ src/converter/blob_manager.hpp
@@ -15,7 +15,6 @@
 ** You should have received a copy of the GNU General Public License
 ** along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ************************************************************************/
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <numeric>
@@ -70,136 +69,12 @@ private:
   ankerl::unordered_dense::map<page_t, std::vector<section_t>> _free_sections_by_page;
 
 public:
-  blob_manager_t()
-    : _next_offset{0}
-  {
-  }
+  blob_manager_t();
 
-  [[nodiscard]] offset_t register_blob(size_type_t size)
-  {
-    for (auto &[page_number, sections] : _free_sections_by_page)
-    {
-      // Find a section that is big enough to hold the size requested
-      auto it = std::find_if(sections.begin(), sections.end(), [&size](const section_t &section) { return section.size.data >= size.data; });
-
-      if (it != sections.end())
-      {
-        // Found a section, adjust its size or remove it if it matches exactly
-        offset_t offset = it->offset;
-
-        if (it->size.data > size.data)
-        {
-          it->offset.data += size.data;
-          it->size.data -= size.data;
-        }
-        else
-        {
-          // Erase by swapping with the last element (to avoid shifting elements)
-          std::swap(*it, sections.back());
-          sections.pop_back();
-        }
-
-        // If no sections left, remove the page
-        if (sections.empty())
-        {
-          _free_sections_by_page.erase(page_number);
-        }
-
-        return offset;
-      }
-    }
-
-    // No free section found in existing pages, allocate a new section at the end
-    offset_t new_offset = _next_offset;
-    _next_offset.data += size.data;
-    return new_offset;
-  }
-
-  [[nodiscard]] bool unregister_blob(offset_t offset, size_type_t size)
-  {
-    // Calculate the start and end page
-    page_t start_page = offset.page();
-    page_t end_page = (offset.data + size.data - 1) / PAGE_SIZE;
-
-    if (offset.data + size.data > _next_offset.data)
-    {
-      return false; // Trying to unregister a segment that goes beyond the allocated space
-    }
-
-    // Iterate over pages that the blob spans
-    for (page_t page = start_page; page <= end_page; ++page)
-    {
-      auto page_it = _free_sections_by_page.find(page);
-      if (page_it == _free_sections_by_page.end())
-      {
-        // If the page doesn't exist, create it
-        page_it = _free_sections_by_page.emplace(page, std::vector<section_t>()).first;
-      }
-
-      auto &sections = page_it->second;
-      // Find the position where the new free section should be inserted
-      auto it = std::lower_bound(sections.begin(), sections.end(), offset, [](const section_t &section, const offset_t &val) { return section.offset.data < val.data; });
-
-      if (it != sections.begin() && (it - 1)->offset.data + (it - 1)->size.data > offset.data)
-      {
-        return false; // Overlaps with the previous free section
-      }
-
-      // Check for overlap with the next section, if it exists
-      if (it != sections.end() && it->offset.data < offset.data + size.data)
-      {
-        return false; // Overlaps with the next free section
-      }
-
-      // If it's not at the beginning, try to merge with the previous section if adjacent
-      if (it != sections.begin() && (it - 1)->offset.data + (it - 1)->size.data == offset.data)
-      {
-        --it;                       // Move iterator to the previous section to merge
-        it->size.data += size.data; // Increase the size of the free section
-        offset = it->offset;        // Update the offset to the beginning of the merged section
-        // Try to merge with the next section if it is adjacent
-        auto next_it = it + 1;
-        if (next_it != sections.end() && offset.data + size.data == next_it->offset.data)
-        {
-          it->size.data += next_it->size.data; // Increase the size of the free section
-          sections.erase(next_it);             // Erase the next section as it is now merged
-        }
-      }
-      else
-      {
-        // If it's at the end or sections is empty, simply append the new free section
-        sections.emplace_back(section_t{offset, size});
-        it = sections.end() - 1; // Update the iterator to point to the new element
-      }
-
-      // Remove the page if no sections left
-      if (sections.empty())
-      {
-        _free_sections_by_page.erase(page_it);
-      }
-    }
-
-    // Adjust _next_offset if the blob was at the end
-    if (offset.data + size.data == _next_offset.data)
-    {
-      _next_offset.data -= size.data;
-    }
-
-    return true;
-  }
-
-  size_t get_free_sections_count()
-  {
-    return std::accumulate(_free_sections_by_page.begin(), _free_sections_by_page.end(), size_t(0), [](size_t sum, const auto &page) { return sum + page.second.size(); });
-  }
-  section_t get_free_section(page_t page, size_t n)
-  {
-    return _free_sections_by_page[page][n];
-  }
-
-  size_type_t get_file_size()
-  {
-    return {_next_offset.data};
-  }
+  [[nodiscard]] offset_t register_blob(size_type_t size);
+  [[nodiscard]] bool unregister_blob(offset_t original_offset, size_type_t size);
+  size_t get_free_sections_count() const;
+  size_t get_pages_count() const;
+  section_t get_free_section(page_t page, size_t n) const;
+  size_type_t get_file_size() const;
 };
-
--- tests/private/blob_manager_test.cpp
+++ tests/private/blob_manager_test.cpp
@@ -245,3 +245,184 @@ TEST_CASE("Unregistering blob overlapping with free section should fail", "[blob
 
   REQUIRE(manager.get_file_size().data == current_file_size.data);
 }
+
+TEST_CASE("Register blobs that fit within a single page", "[blob_manager_t]") {
+    blob_manager_t manager;
+
+    // Assuming PAGE_SIZE is large enough for these blobs.
+    REQUIRE_NOTHROW(manager.register_blob({blob_manager_t::PAGE_SIZE / 4}));
+    REQUIRE_NOTHROW(manager.register_blob({blob_manager_t::PAGE_SIZE / 4}));
+    REQUIRE_NOTHROW(manager.register_blob({blob_manager_t::PAGE_SIZE / 4}));
+    REQUIRE_NOTHROW(manager.register_blob({blob_manager_t::PAGE_SIZE / 4}));
+
+    REQUIRE(manager.get_free_sections_count() == 0); // No unregistration has happened yet.
+}
+
+TEST_CASE("Register and unregister blobs causing overflow on pages", "[blob_manager_t]") {
+    blob_manager_t manager;
+
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE - 1});
+    auto offset2 = manager.register_blob({2}); // This should cause an overflow to the next page
+
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE - 1}) == true);
+    REQUIRE(manager.unregister_blob(offset2, {2}) == true);
+
+    REQUIRE(manager.get_free_sections_count() == 0); // There should be two free sections, one on each page
+}
+
+TEST_CASE("Unregister blobs to create a completely free page", "[blob_manager_t]") {
+    blob_manager_t manager;
+
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE / 2});
+    auto offset2 = manager.register_blob({blob_manager_t::PAGE_SIZE / 2});
+
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE / 2}) == true);
+    REQUIRE(manager.unregister_blob(offset2, {blob_manager_t::PAGE_SIZE / 2}) == true);
+
+    REQUIRE(manager.get_free_sections_count() == 0); 
+}
+
+TEST_CASE("Unregister blobs that span multiple pages", "[blob_manager_t]") {
+    blob_manager_t manager;
+
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE / 2});
+    auto offset2 = manager.register_blob({blob_manager_t::PAGE_SIZE}); // This blob spans two pages
+
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE / 2}) == true);
+    REQUIRE(manager.unregister_blob(offset2, {blob_manager_t::PAGE_SIZE}) == true);
+
+    REQUIRE(manager.get_free_sections_count() == 0);
+}
+
+TEST_CASE("Edge case: Unregister a blob at the boundary of two pages", "[blob_manager_t]") {
+    blob_manager_t manager;
+
+    auto offset = manager.register_blob({blob_manager_t::PAGE_SIZE - 1});
+    auto offset2 = manager.register_blob({1});
+    auto offset3 = manager.register_blob({1});
+    auto offset4 = manager.register_blob({1});
+    auto offset5 = manager.register_blob({1});
+
+    REQUIRE(manager.unregister_blob(offset, {blob_manager_t::PAGE_SIZE - 1}) == true);
+    REQUIRE(manager.get_free_sections_count() == 1);
+    REQUIRE(manager.unregister_blob(offset2, {1}) == true);
+    REQUIRE(manager.get_free_sections_count() == 1);
+    REQUIRE(manager.unregister_blob(offset3, {1}) == true);
+    REQUIRE(manager.get_free_sections_count() == 2);
+    REQUIRE(manager.unregister_blob(offset4, {1}) == true);
+    REQUIRE(manager.get_free_sections_count() == 2);
+    REQUIRE(manager.unregister_blob(offset5, {1}) == true);
+    REQUIRE(manager.get_free_sections_count() == 0);
+}
+
+TEST_CASE("Registering blob that spans multiple pages", "[blob_manager_t]")
+{
+    blob_manager_t manager;
+
+    SECTION("Register blob spanning across pages")
+    {
+    auto large_blob_size = uint64_t(blob_manager_t::PAGE_SIZE * 1.5);
+    auto offset = manager.register_blob({large_blob_size});
+    REQUIRE(offset.page() == 0);                                          // Starts on the first page
+    REQUIRE((offset.data + large_blob_size) > blob_manager_t::PAGE_SIZE); // Spans to the next page
+    }
+}
+
+TEST_CASE("Unregistering blob that spans multiple pages", "[blob_manager_t]")
+{
+    blob_manager_t manager;
+    auto large_blob_size = uint64_t(blob_manager_t::PAGE_SIZE * 1.5);
+    auto offset = manager.register_blob({large_blob_size});
+
+    SECTION("Unregister blob spanning across pages")
+    {
+    REQUIRE(manager.unregister_blob(offset, {large_blob_size}) == true);
+    auto free_section_count = manager.get_free_sections_count();
+    REQUIRE(free_section_count == 0); // Should have two free sections, one on each page
+    }
+}
+
+TEST_CASE("Page removal upon blob unregistration", "[blob_manager_t]") {
+  blob_manager_t manager;
+
+  SECTION("Unregister all blobs on a page") {
+    auto offset = manager.register_blob({blob_manager_t::PAGE_SIZE});
+    REQUIRE(manager.unregister_blob(offset, {blob_manager_t::PAGE_SIZE}) == true);
+    REQUIRE(manager.get_pages_count() == 0); // The page should be removed
+  }
+}
+
+TEST_CASE("Page addition upon blob registration", "[blob_manager_t]") {
+  blob_manager_t manager;
+
+  SECTION("Register blob causing new page to be added") {
+    auto offset = manager.register_blob({blob_manager_t::PAGE_SIZE + 2});
+    manager.unregister_blob(offset, {blob_manager_t::PAGE_SIZE + 1});
+    REQUIRE(manager.get_pages_count() == 2); // Should now track more than one page
+  }
+}
+
+TEST_CASE("Merging across pages", "[blob_manager_t]") {
+  blob_manager_t manager;
+
+  SECTION("Unregister blobs to create non-contiguous free space across pages") {
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE - 1});
+    auto offset2 = manager.register_blob({2}); // This will start at the end of page 0 and spill over to page 1
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE - 1}) == true);
+    REQUIRE(manager.unregister_blob(offset2, {2}) == true);
+    REQUIRE(manager.get_pages_count() == 0); // Ensure that two pages are tracked separately
+  }
+}
+
+TEST_CASE("Fragmentation within a page", "[blob_manager_t]") {
+  blob_manager_t manager;
+
+  SECTION("Create fragmented free space within a page") {
+    auto offset1 = manager.register_blob({10});
+    auto offset2 = manager.register_blob({10});
+    auto offset3 = manager.register_blob({blob_manager_t::PAGE_SIZE - 20});
+    REQUIRE(manager.unregister_blob(offset1, {10}) == true);
+    REQUIRE(manager.unregister_blob(offset2, {10}) == true);
+    auto offset4 = manager.register_blob({20}); // This should fit into the fragmented space
+    REQUIRE(offset4.data == offset1.data); // Should occupy the space from the first unregistered blob
+  }
+}
+
+TEST_CASE("Handling of full pages", "[blob_manager_t]") {
+  blob_manager_t manager;
+
+  SECTION("Completely fill a page then unregister some blobs") {
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE / 2});
+    auto offset2 = manager.register_blob({blob_manager_t::PAGE_SIZE / 2});
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE / 2}) == true);
+    REQUIRE(manager.unregister_blob(offset2, {blob_manager_t::PAGE_SIZE / 2}) == true);
+    REQUIRE(manager.get_pages_count() == 0); // The page should have been removed
+  }
+}
+
+TEST_CASE("Consecutive page filling and freeing", "[blob_manager_t]") {
+  blob_manager_t manager;
+
+  SECTION("Fill and free multiple pages consecutively") {
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE});
+    auto offset2 = manager.register_blob({blob_manager_t::PAGE_SIZE});
+    REQUIRE(manager.unregister_blob(offset2, {blob_manager_t::PAGE_SIZE}) == true);
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE}) == true);
+    REQUIRE(manager.get_pages_count() == 0); // Both pages should have been removed
+  }
+}
+
+TEST_CASE("Sparse free sections on multiple pages", "[blob_manager_t]") 
+{
+  blob_manager_t manager;
+
+  SECTION("Create non-contiguous free sections across multiple pages") {
+    auto offset1 = manager.register_blob({blob_manager_t::PAGE_SIZE / 3});
+    auto offset2 = manager.register_blob({blob_manager_t::PAGE_SIZE / 3});
+    auto offset3 = manager.register_blob({blob_manager_t::PAGE_SIZE / 3});
+    REQUIRE(manager.unregister_blob(offset1, {blob_manager_t::PAGE_SIZE / 3}) == true);
+    REQUIRE(manager.unregister_blob(offset3, {blob_manager_t::PAGE_SIZE / 3}) == true);
+    auto offset4 = manager.register_blob({blob_manager_t::PAGE_SIZE / 3}); // Should fit into the first free section
+    REQUIRE(offset4.data == offset1.data);
+  }
+}
